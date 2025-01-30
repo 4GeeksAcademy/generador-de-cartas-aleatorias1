94036099d4ab670aed916275261bb1f529fee217
alex15328 <alexmen15328@gmail.com>
2025-01-30 19:56:10 +0000

    Generador de cartas aleatorias1

--- src/app.js
+++ src/app.js
@@ -8,4 +8,45 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Chau prettier!!");
+
+  console.log("Escoge una carta aleatoria");
+  function generarcarta() {
+
+  
+  const palos =["♦", "♥", "♠", "♣"];
+  const valores = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ]
+  const suerte = palos[Math.floor(Math.random() * palos.length)];
+  const numero = valores[Math.floor(Math.random() * valores.length)];
+  let elementoConElValorCarta = document.querySelector(".valor");
+  elementoConElValorCarta.innerHTML = numero;
+  let elementosQueTienenElPalo = document.querySelectorAll(".palo");
+  for (const div of elementosQueTienenElPalo) {
+  div.innerHTML = suerte;
+  
+  if (suerte=="♥" || suerte=="♦") {
+    div.style.color= "red";
+  } else if (suerte=="♣" || suerte=="♠"){
+    div.style.color= "black";
+  }
+}
+}
+generarcarta()
+document.querySelector("#click").addEventListener("click",() =>{
+  generarcarta()
+})
 };
+
